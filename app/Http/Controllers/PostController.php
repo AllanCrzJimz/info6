4f6959226c347d6c339b530d697e3abfd2fbfea5
AllanCrzJimz <lazywolfk@gmail.com>
2022-03-17 21:24:59 -0600

    Formulario Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Posts;
 
 class PostController extends Controller
 {
@@ -39,6 +40,13 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
+        $validated= $request->validate([
+            'tittle' => 'required | min:5 | max:500',
+            'url_clean' => 'required'
+        ]);
+        //dd($validated);
+    
+    Posts::create($validated);
     }
 
     //Usado para consultas, muestra informacion//
@@ -77,6 +85,7 @@ class PostController extends Controller
     public function update(Request $request, $id)
     {
         //
+
     }
 
     /**
--- resources/views/dashboard/posts.blade.php
+++ resources/views/dashboard/posts.blade.php
@@ -1,31 +1,17 @@
 @extends('layouts.master')
 
 @section('content') 
-        <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <div id="emailHelp" class="form-text">Well never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-
-<form action="" method="post">
+@include('fragments.validation-errors')
+<h1>Post</h1>
+<form method="post" action="{{route("post.store")}}" >
+    @csrf
     <div class="form-group">
         <label for="tittle">Titulo</label>
         <input type="text" class="form-control" name="tittle" id="tittle">
     </div>
 
     <div class="form-group">
-        <label for="url_clean">Url Limpia</label>
+        <label for="url_clean">URL</label>
         <input type="text" class="form-control" name="url_clean">
     </div>
 
@@ -34,9 +20,10 @@
         <textarea name="form-control" id="content" name="content" rows="3"></textarea>
     </div>
 
-<input type="submmit" value="Enviar" class="btn btn-primary">
+<button type="submmit" value="Enviar" class="btn btn-primary">Submit</button>
 
 </form>
+
 
 
 
